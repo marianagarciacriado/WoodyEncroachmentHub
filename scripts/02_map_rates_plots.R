24189edbec1043fed8864d7f637adadaeb52f90b
Mariana García Criado <s1771678@ed.ac.uk>
2020-01-03 15:30:42 +0000

    Mariana cleaned Script 02

--- scripts/02_map_rates_plots.R
+++ scripts/02_map_rates_plots.R
@@ -1,24 +1,20 @@
 #### Woody encroachment across biomes 
-#### Script 2. Woody cover change map + density plots
-#### Mariana Garcia 
-#### November 2017
+#### Script 02. Woody cover change map & rates
+#### Mariana García Criado 
+#### March 2019
 
-## Packages ----
-.libPaths("C:/R_library")
 
-library(maps)
-library(mapdata)
-library(mapproj)
+## LIBRARIES ----
 library(proj4)
 library(scales)
 library(ggalt)
 library(ggplot2)
 library(dplyr)
 library(cowplot)
-library(tidyverse)
 library(MCMCglmm)
 
-## Define theme ----
+
+## THEME ----
 dense.theme <- theme(axis.title.x = element_text(face="bold", size=34),
                      axis.text.x  = element_text(vjust=0.5, size=28, colour = "black"), 
                      axis.title.y = element_text(face="bold", size=34),
@@ -31,10 +27,9 @@ dense.theme <- theme(axis.title.x = element_text(face="bold", size=34),
                      legend.text = element_text(size=34), legend.key = element_blank(),
                      plot.margin = unit(c(1,1,1,1), units = , "cm"))
 
-## Loading data ----
-cover.msc <- read.csv("scripts/users/mgarciacriado/encroachment_paper/final_scripts/mastersheets/cover_ms_clean.csv")
 
-## Data prep ----
+## DATA LOAD & PREP ----
+cover.msc <- read.csv("mastersheets/cover_ms_clean.csv")
 
 # Re-scaling the values so the points of change are proportional
 cover.msc$Absolute_change <- abs(cover.msc$Annual.rate)
@@ -48,7 +43,8 @@ tundra_decrease <- filter(cover.msc, Biome_trend == "Tundra_decrease")
 tundra_increase <- filter(cover.msc, Biome_trend == "Tundra_increase")
 tundra_stable <- filter(cover.msc, Biome_trend == "Tundra_stable")
 
-## Create map of woody cover change ---- 
+
+## MAP OF WOODY COVER CHANGE ---- 
 world <- map_data("world")
 world <- world[world$region != "Antarctica",]
 
@@ -68,7 +64,8 @@ world <- world[world$region != "Antarctica",]
           axis.title.x=element_blank(), legend.key = element_blank(), 
           panel.background = element_blank(), panel.border = element_blank()))
 
-#### Density plots of woody cover change ----
+
+## RATES OF WOODY COVER CHANGE ----
 
 ## Savanna density plot
 dp_sav <- filter(cover.msc, Biome_type == "Savanna")
@@ -112,9 +109,9 @@ head(m_tun)
     dense.theme)
 
 
-## Figure 1. Map and density plots ----
+## FIGURE 1 ----
 
-# Vertical alignment with cowplot
+# Map & density plots figure 
 (map_dens_panel <- cowplot::ggdraw() +
     draw_plot(cover.map, x = -0.2, y = -0.05, width = 0.9, height = 1) +
     draw_plot(dp.tun, x = 0.6, y = 0.5, width = 0.35, height = 0.5) +
@@ -122,141 +119,145 @@ head(m_tun)
     draw_plot_label(label = c("(a)", "(b)", "(c)"), size = 38,
                     x = c(0.01, 0.6, 0.6), y = c(0.95, 0.95, 0.5)))
 
-ggplot2::ggsave(map_dens_panel, filename = "scripts/users/mgarciacriado/encroachment_paper/final_scripts/figures/Figure_1.png", 
+# Save Figure 1
+ggplot2::ggsave(map_dens_panel, filename = "figures/Figure_1.png", 
                 width = 80, height = 35, units = "cm")
 
 
-## Rates models and means ----
+## TRENDS PER BIOME ----
 
-## Data filtering 
+## Filtering per cover trend 
 total_inc <- filter(cover.msc, Trend == "Increase")
 total_dec <- filter(cover.msc, Trend == "Decrease")
 total_stab <- filter(cover.msc, Trend == "Stable")
 
-## Calculate percentages per trend and biome ----
 
-# Percentage of increase records in T&S
+## Percentage of records per trend and biome 
+
+# Percentage of increasing records per biome
 pc_inc_tt <- (length(tundra_increase$Plot.ID)/length(cover.msc$Plot.ID))*100
-pc_inc_tt #19.56% records of the total records (savannah+tundra) are increasing tundra records
+pc_inc_tt #19.56% records of the total records (savanna+tundra) are increasing tundra records
 
 pc_inc_t <- (length(tundra_increase$Plot.ID)/length(dp_tun$Plot.ID))*100
-pc_inc_t #68.05% records of tundra records are increasing tundra records (TEXT)
+pc_inc_t #68.05% records of tundra records are increasing tundra records
 
 pc_inc_st <- (length(savanna_increase$Plot.ID)/length(cover.msc$Plot.ID))*100
-pc_inc_st #48.3% records of the total records (savannah+tundra) are increasing savannah records
+pc_inc_st #48.3% records of the total records (savanna+tundra) are increasing savanna records
 
 pc_inc_s <- (length(savanna_increase$Plot.ID)/length(dp_sav$Plot.ID))*100
-pc_inc_s #67.78% records of savanna records are increasing savanna records (TEXT)
+pc_inc_s #67.78% records of savanna records are increasing savanna records
 
 
-# Percentage of decrease records in T&S
+# Percentage of decreasing records per biome
 pc_dec_tt <- (length(tundra_decrease$Plot.ID)/length(cover.msc$Plot.ID))*100
-pc_dec_tt #5.96% records of the total records (savannah+tundra) are decreasing tundra records
+pc_dec_tt #5.96% records of the total records (savanna+tundra) are decreasing tundra records
 
 pc_dec_t <- (length(tundra_decrease$Plot.ID)/length(dp_tun$Plot.ID))*100
 pc_dec_t #20.76% records of tundra records are decreasing tundra records
 
 pc_dec_st <- (length(savanna_decrease$Plot.ID)/length(cover.msc$Plot.ID))*100
-pc_dec_st #18.82% records of the total records (savannah+tundra) are decreasing savannah records
+pc_dec_st #18.82% records of the total records (savanna+tundra) are decreasing savanna records
 
 pc_dec_s <- (length(savanna_decrease$Plot.ID)/length(dp_sav$Plot.ID))*100
 pc_dec_s #26.41% records of savanna records are decreasing savanna records
 
 
-# Percentage of stable records in T&S
+# Percentage of stable records per biome
 pc_sta_tt <- (length(tundra_stable$Plot.ID)/length(cover.msc$Plot.ID))*100
-pc_sta_tt #3.21% records of the total records (savannah+tundra) are stable tundra records
+pc_sta_tt #3.21% records of the total records (savanna+tundra) are stable tundra records
 
 pc_sta_t <- (length(tundra_stable$Plot.ID)/length(dp_tun$Plot.ID))*100
 pc_sta_t #11.18% records of tundra records are stable tundra records
 
 pc_sta_st <- (length(savanna_stable$Plot.ID)/length(cover.msc$Plot.ID))*100
-pc_sta_st #4.13% records of the total records (savannah+tundra) are stable savannah records
+pc_sta_st #4.13% records of the total records (savanna+tundra) are stable savanna records
 
 pc_sta_s <- (length(savanna_stable$Plot.ID)/length(dp_sav$Plot.ID))*100
 pc_sta_s #5.79% records of savanna records are stable savanna records
 
 
-## Calculate mean value per trend and biome ----
+## Mean values per trend and biome
 
-# Mean increase value in T&S
+# Mean increasing value per biome
 mean_inc_t <- tundra_increase %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_inc_t # 0.37% ± 0.5 is the mean increase value in the tundra (TEXT)
+mean_inc_t # 0.37% ± 0.5 is the mean increase value in the tundra 
 
 mean_inc_s <- savanna_increase %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_inc_s # 0.63 ± 0.82 is the mean increase value in the savanna (TEXT)
+mean_inc_s # 0.63 ± 0.82 is the mean increase value in the savanna
 
 
-# Mean stable value in T&S
+# Mean stable value per biome
 mean_stab_t <- tundra_stable %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_stab_t # 0.001% ± 0.003 is the mean stable value in the tundra (TEXT)
+mean_stab_t # 0.001% ± 0.003 is the mean stable value in the tundra 
 
 mean_stab_s <- savanna_stable %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_stab_s # 0.0003 ± 0.005% is the mean stable value in the savanna (TEXT)
+mean_stab_s # 0.0003 ± 0.005% is the mean stable value in the savanna
 
 
-# Mean decrease value in T&S
+# Mean decreasing value per biome
 mean_dec_t <- tundra_decrease %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_dec_t # -0.36% ± 0.75 is the mean decrease value in the tundra (TEXT)
+mean_dec_t # -0.36% ± 0.75 is the mean decrease value in the tundra 
 
 mean_dec_s <- savanna_decrease %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_dec_s # -0.4 ± 0.45 is the mean decrease value in the savanna (TEXT)
+mean_dec_s # -0.4 ± 0.45 is the mean decrease value in the savanna
 
 
-## Calculate overall absolute cover value per biome ----
+## Overall absolute cover value per biome
 
-# Mean value in the tundra (overall)
+# Mean cover change value in the tundra (overall)
 mean_tun <- dp_tun %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_tun # 0.18% ± 0.61 is the mean of cover values in the tundra (TEXT)
+mean_tun # 0.18% ± 0.61 is the mean of cover values in the tundra 
 
-# Mean value in the savanna (overall)
+# Mean cover change value in the savanna (overall)
 mean_sav <- dp_sav %>% summarise(mean = mean(Annual.rate), sd = sd(Annual.rate))
-mean_sav # 0.32% ± 0.85 is the mean of cover values in the savannah (TEXT)
+mean_sav # 0.32% ± 0.85 is the mean of cover values in the savanna
 
 
-## Models to test differences in means
+## MODELS IN COVER CHANGE DIFFERENCES ----
+
+# Differences in increasing cover trends between biomes
 inc.means.mod <- MCMCglmm(Annual.rate ~ Biome_type, data = total_inc, 
                              nitt = 100000, burnin = 5000, thin = 30)
-summary(inc.means.mod)
-# significantly different increase values, CIs don't overlap
-save(inc.means.mod, file = "scripts/users/mgarciacriado/encroachment_paper/final_scripts/models/fixed/inc.means.mod.RData")
+summary(inc.means.mod) # significantly different increase values
+save(inc.means.mod, file = "models/fixed/inc.means.mod.RData")
 
+# Differences in stable cover trends between biomes
 stab.means.mod <- MCMCglmm(Annual.rate ~ Biome_type, data = total_stab, 
                              nitt = 100000, burnin = 5000, thin = 30)
-summary(stab.means.mod)
-# stables are not significantly different, CIs overlap
-save(stab.means.mod, file = "scripts/users/mgarciacriado/encroachment_paper/final_scripts/models/fixed/stab.means.mod.RData")
+summary(stab.means.mod) # not significantly different
+save(stab.means.mod, file = "models/fixed/stab.means.mod.RData")
 
+# Differences in decreasing cover trends between biomes
 dec.means.mod <- MCMCglmm(Annual.rate ~ Biome_type, data = total_dec, 
                              nitt = 100000, burnin = 5000, thin = 30)
-summary(dec.means.mod)
-# decreases are not significantly different, CIs overlap
-save(dec.means.mod, file = "scripts/users/mgarciacriado/encroachment_paper/final_scripts/models/fixed/dec.means.mod.RData")
+summary(dec.means.mod) # not significantly different
+save(dec.means.mod, file = "models/fixed/dec.means.mod.RData")
 
+# Differences in overall cover trends between biomes
 overall.mod <- MCMCglmm(Annual.rate ~ Biome_type, data = cover.msc, 
                     nitt = 100000, burnin = 5000, thin = 30)
-summary(overall.mod)
-save(overall.mod, file = "scripts/users/mgarciacriado/encroachment_paper/final_scripts/models/fixed/overall.mod.RData")
-# overall values are significantly different between biomes
+summary(overall.mod) # significantly different overall values
+save(overall.mod, file = "models/fixed/overall.mod.RData")
 
 
-#### Geographical location ----
 
-## Percentage records per trend and continent - increases
+## TRENDS PER CONTINENT ----
+
+# Percentage records per trend and continent - increases
 inc.cont.t <- tundra_increase %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(total_inc$Plot.ID))*100)
 
 inc.cont.s <- savanna_increase %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(total_inc$Plot.ID))*100)
 
-## Percentage records per trend and continent - stables
+# Percentage records per trend and continent - stables
 stab.cont.t <- tundra_stable %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(total_stab$Plot.ID))*100)
 
 stab.cont.s <- savanna_stable %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(total_stab$Plot.ID))*100)
 
-## Percentage records per trend and continent - decreases
+# Percentage records per trend and continent - decreases
 dec.cont.t <- tundra_decrease %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(total_dec$Plot.ID))*100)
 
@@ -274,32 +275,26 @@ dec.cont <- total_dec %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(cover.msc$Plot.ID))*100)
 
 
-## Reported records in the tundra
+# Reported records in the tundra
 continent.tun <- dp_tun %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(dp_tun$Plot.ID))*100)
 
-## Reported records in the savanna
+# Reported records in the savanna
 continent.sav <- dp_sav %>% group_by(Continent) %>% count() %>%
   mutate(percent = (n/length(dp_sav$Plot.ID))*100)
 
 
-
-
-## Largest number of increasing records relative to each continent ----
+## Increasing records relative to each continent
 relative <- cover.msc %>% group_by(Continent) %>% count() %>% rename(total = n)
 
+# Number of increasing records per continent
 inc.cont.relat <- cover.msc %>% group_by(Continent) %>%
   tally(Trend == "Increase")
 
+# Calculate percentage
 inc.relative <- merge(relative, inc.cont.relat, by = "Continent") %>% mutate(percent = (n/total)*100)
 inc.relative
 
 
-
-
-## Number of records captured by different methods ----
+## NUMBER OF RECORDS PER METHOD ----
 method.sum <- cover.msc %>% group_by(Method) %>% count(Method)
-
-## Number of records before 1979 and after 2013 (available climatic timeframe) ----
-start.1919 <- filter(cover.msc, Start_year < 1979) #230 records
-end.2013 <- filter(cover.msc, End_year > 2013) #344 records
